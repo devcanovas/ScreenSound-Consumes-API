ed80dcb1bfe48db5a7f4f5806c4dd8db92e3ad10
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-20 14:41:30 -0300

    chore!: Uploading the project

---
+++ Models/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text.Json.Serialization;
+
+namespace ScreenSound_Consumes_API.Models;
+
+internal class Music
+{
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int? Duration { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+
+    public void ShowMusicDetails()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Artist: {Artist}");
+        Console.WriteLine($"Durantion: {Duration}");
+        Console.WriteLine($"Genre: {Genre}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSound_Consumes_API.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musics = JsonSerializer.Deserialize<List<Music>>(response)!;
+        musics?.ForEach(music => music.ShowMusicDetails());
+        Console.WriteLine(musics?.Count);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema! {ex.Message}");
+    }
+}
