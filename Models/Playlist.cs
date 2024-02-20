2c4aa02d29be18aaea71e5c020659dda728be581
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-20 16:43:18 -0300

    feat: Implementing Playlist Feature

---
+++ Models/Playlist.cs
@@ -0,0 +1,19 @@
+﻿namespace ScreenSound_Consumes_API.Models;
+
+internal class Playlist(string Name)
+{
+    public string Name { get; set; } = Name;
+    public List<Music> musics { get; } = [];
+
+    public void AddMusic(Music music)
+    {
+        musics.Add(music);
+    }
+
+    public void GetPlaylistDetails()
+    {
+        Console.WriteLine($"Nome da playlist: {Name}\n");
+        Console.WriteLine("Músicas adicionadas:");
+        musics.ForEach(music => Console.WriteLine($"- {music.Name}"));
+    }
+}
--- obj/Debug/net8.0/ScreenSound-Consumes-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScreenSound-Consumes-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound-Consumes-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7328f3660ed0de1f2a4f32457370a64adb4ba2f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9bc2692ac3bbe4bd2fcd8bd713243b5401bfbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound-Consumes-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound-Consumes-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
