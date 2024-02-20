186e7bfbbe7f5a264bfe65b5fd59722dd3d752aa
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-20 14:51:41 -0300

    feat: Implementing Movies on API

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
-namespace ScreenSound_Consumes_API.Models
+namespace ScreenSound_Consumes_API.Models;
+
+internal class Movie
 {
-    internal class Movie
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("crew")]
+    public string? Crew { get; set; }
+
+    [JsonPropertyName("fullTitle")]
+    public string? FullTitle { get; set; }
+
+    [JsonPropertyName("year")]
+    public string? Year { get; set; }
+
+    public void ShowMovieDetails()
     {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Crew: {Crew}");
+        Console.WriteLine($"Full Title: {FullTitle}");
+        Console.WriteLine($"Year: {Year}\n");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using ScreenSound_Consumes_API.Models;
 using System.Text.Json;
 
-using (HttpClient client = new HttpClient())
+HttpClient client = new();
+async Task RequestMusicAsync()
 {
     try
     {
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        var musics = JsonSerializer.Deserialize<List<Music>>(response)!;
+        List<Music> musics = JsonSerializer.Deserialize<List<Music>>(response)!;
         musics?.ForEach(music => music.ShowMusicDetails());
         Console.WriteLine(musics?.Count);
     }
@@ -15,3 +16,20 @@ using (HttpClient client = new HttpClient())
         Console.WriteLine($"Temos um problema! {ex.Message}");
     }
 }
+
+async Task RequestMovieAsync()
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+        List<Movie> movies = JsonSerializer.Deserialize<List<Movie>>(response)!;
+        movies?.ForEach(movie => movie.ShowMovieDetails());
+        Console.WriteLine(movies?.Count);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine($"Ops! tivemos um problema {e.Message}");
+    }
+}
+
+await RequestMovieAsync();
